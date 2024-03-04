3cf26c7bbb917ee11421bd58bff90786156eba0e
tranlehienthuc <156295827+tranlehienthuc@users.noreply.github.com>
2024-03-04 18:15:24 +0700

    Delete bai 1 buoi 3.py

--- bai 1 buoi 3.py
+++
@@ -1,4 +0,0 @@
-shoppinglist = ['MILK','CHEESE','BUTTER']# Tạo một danh sách chứa các mặt hàng cần mua
-
-for i in shoppinglist:# Duyệt qua từng mục trong danh sách và in ra màn hình
-    print(i) # In giá trị của mục hiện tại ra màn hình
