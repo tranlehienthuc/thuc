e713bfb0a032f9ad588c146e783058eb1b9e2b7c
tranlehienthuc <n21dcdt087@student.ptithcm.edu.vn>
2024-03-04 18:04:28 +0700

    first commit

---
+++ bai 1 buoi 3.py
@@ -0,0 +1,4 @@
+shoppinglist = ['MILK','CHEESE','BUTTER']# Tạo một danh sách chứa các mặt hàng cần mua
+
+for i in shoppinglist:# Duyệt qua từng mục trong danh sách và in ra màn hình
+    print(i) # In giá trị của mục hiện tại ra màn hình
